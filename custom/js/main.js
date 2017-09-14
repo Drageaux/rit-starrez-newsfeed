4420dcdc3681bb337db63d11f562ef3a85dbfaf1
David Thong Nguyen <thnguyen2808@gmail.com>
2017-09-14 17:19:27 -0400

    Added back the collapse navbar sizing

--- custom/js/main.js
+++ custom/js/main.js
@@ -1,3 +1,12 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
 $(document).ready(function () {
     $('#collapse-nav .item').tab({
         history: true,
